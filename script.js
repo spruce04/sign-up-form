2f1e58ba7f40ac40882331f9568f83b652e4f757
Sam Flax <sam.flax2005@gmail.com>
2022-09-25 00:00:43 +1000

    finished first draft

---
+++ script.js
@@ -0,0 +1,17 @@
+//script to check if passwords are equal
+const passOne = document.getElementById('password');
+const passTwo = document.getElementById('confirmed');
+const button = document.getElementById('submitData');
+const targetDiv = document.getElementById('notEqual')
+
+//to be called if passwords are not identical
+function checkEquality () {
+    if (passOne.value == passTwo.value) {
+        targetDiv.textContent = ''
+        return true;
+    }
+    else {
+        targetDiv.textContent = '(passwords do not match)';
+        return false;
+    }
+}
